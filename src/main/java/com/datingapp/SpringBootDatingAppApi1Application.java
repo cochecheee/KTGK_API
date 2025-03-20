f352b9211f3321422ca6ed93f903764c22ed5abc
tien.blt <buitien747@gmail.com>
2025-03-20 12:54:40 +0700

    api

---
+++ src/main/java/com/datingapp/SpringBootDatingAppApi1Application.java
@@ -0,0 +1,13 @@
+package com.datingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatingAppApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatingAppApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/datingapp/SpringBootDatingAppApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatingAppApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
